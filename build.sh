65e363fc30e1868767188bcc05064c5fa6b641e7
Indu <indhubharathi@gmail.com>
2019-07-30 08:08:22 +0000

    Build tensorflow with rdynamic

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel build --linkopt=-rdynamic --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
