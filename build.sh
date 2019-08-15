27a6d199178056e5fa5b89b6ac5ee94cba04db92
Indu <indhubharathi@gmail.com>
2019-08-15 16:32:03 +0000

    Build with debug info

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-bazel build --linkopt=-rdynamic --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel build --copt=-O1 -c dbg -c opt --linkopt=-rdynamic --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package 
